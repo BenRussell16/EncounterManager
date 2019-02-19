6c8ae742f80f54660b45dacb0513410ddb9f7c11
russelbenj2 <ben+github@hardknocks.school.nz>
2019-02-19 23:36:47 +1300

    Allowed filtering the spell list, and adding new spells to the temporary list.
    Applied query filters to the spell list.
    Added inputs for adding a spell, and added it to the local spell list.
    TODO - fix source filtering. commit to the archive.

--- EncounterManager/src/Spells/SpellBuilder.java
+++ EncounterManager/src/Spells/SpellBuilder.java
@@ -1,22 +1,14 @@
 package EncounterManager.src.Spells;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import EncounterManager.Resources.Source;
-import EncounterManager.src.Creatures.Creature;
-import EncounterManager.src.Encounters.EncounterEntity;
-import EncounterManager.src.Encounters.EncounterManager;
 import EncounterManager.src.Spells.Spell.Classes;
 import EncounterManager.src.Spells.Spell.School;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -24,11 +16,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+@SuppressWarnings("restriction")
 public class SpellBuilder {
 	private final List<Spell> spells;
 	private Stage secondaryStage = null;
@@ -39,19 +31,14 @@ public class SpellBuilder {
 	
 	private List<Spell> query;
 	private GridPane spellList;
+		private List<Label> names;
+		private List<Label> levels;
 	private GridPane curSpell;
-	//TODO - delete if unneeded
-//		private TextField nameField;
-//		private ChoiceBox<Integer> levelPicker;
-//		private ChoiceBox<School> schoolPicker;
-//		private ToggleGroup classRadios;
-//		private ToggleGroup sourceRadios;
-	
-	private Map<TextField,Label> inputs;
-	private List<TextField> fields;
-	private List<Label> fieldTags;
-	private GridPane multiplierTable;
-	private List<Button> party;
+  		private TextField nameField;
+  		private ChoiceBox<Integer> levelPicker;
+  		private ChoiceBox<School> schoolSelect;
+  		private RadioButton c1, c2, c3, c4, c5, c6, c7, c8;
+  		private ChoiceBox<Source> sourceSelect;
 	
 	public Stage makeDisplay() {
 		if(secondaryStage != null) {secondaryStage.close();}
@@ -64,12 +51,10 @@ public class SpellBuilder {
       		query = new ArrayList<Spell>();
       		query.addAll(spells);
 			List<Label> amounts = new ArrayList<Label>();
-			fields = new ArrayList<TextField>();
-			fieldTags = new ArrayList<Label>();
-			inputs = new HashMap<TextField,Label>();
+			names = new ArrayList<Label>();
+			levels = new ArrayList<Label>();
 			
 			GridPane topBar = new GridPane();
-			
 			//TODO make a button to save new spells
 //				Button export = new Button("Export enounter");
 //				export.setOnAction(new EventHandler<ActionEvent>() {
@@ -98,7 +83,7 @@ public class SpellBuilder {
 				TextField nameFilter = new TextField();
 
 				//TODO - allow selection of multiple
-				ChoiceBox<Integer> levelFilter = new ChoiceBox<Integer>(FXCollections.observableArrayList(0,1,2,3,4,5,6,7,8,9));
+				ChoiceBox<Integer> levelFilter = new ChoiceBox<Integer>(FXCollections.observableArrayList(null,0,1,2,3,4,5,6,7,8,9));
 				levelFilter.setValue(null);
 				
 				ChoiceBox<School> schoolPicker = new ChoiceBox<School>(FXCollections.observableArrayList());
@@ -156,6 +141,7 @@ public class SpellBuilder {
 							for(Spell s:toRemove) {query.remove(s);}
 						}
 						if(sourcePicker.getValue()!=null) {//source filter
+							//TODO - fix
 							List<Spell> toRemove = new ArrayList<Spell>();
 							for(Spell s:query) {
 								if(!s.fromSource(sourcePicker.getValue())) {
@@ -183,6 +169,8 @@ public class SpellBuilder {
 			
 
 			
+			
+			//1st pane for spell list
       		ScrollPane sp = new ScrollPane();//allow scrolling down the spell list
       		spellList = new GridPane();
       		spellList.setHgap(10);
@@ -194,38 +182,153 @@ public class SpellBuilder {
 	      		label = new Label(" "+spells.get(i).getName());
 	      		Tooltip toolTip = new Tooltip(spells.get(i).toString());
 	      		label.setTooltip(toolTip);
-	      		fieldTags.add(label);
+	      		names.add(label);
 	      		spellList.add(label, 0, i+1);
 	      		label = new Label(" "+spells.get(i).getLevel());
+	      		levels.add(label);
 	      		spellList.add(label, 1, i+1);
 	      	}
 	      	sp.setContent(spellList);
 	      	grid.add(sp,0,1);
 
-	      	//TODO - 2nd pane for spell addition
+	      	
+	      	
+	      	//2nd pane for spell addition
+	      	curSpell = new GridPane();
+	      	curSpell.setHgap(10);
 
+	      	label = new Label(" Name");
+	      	curSpell.add(label, 0, 0);
+	      	nameField = new TextField();
+	      	curSpell.add(nameField, 1, 0);
+
+	      	label = new Label(" Level");
+	      	curSpell.add(label, 0, 1);
+	    	levelPicker = new ChoiceBox<Integer>(FXCollections.observableArrayList(null,0,1,2,3,4,5,6,7,8,9));
+	    	levelPicker.setValue(null);
+	      	curSpell.add(levelPicker, 1, 1);
+
+	      	label = new Label(" School");
+	      	curSpell.add(label, 0, 2);
+			schoolSelect = new ChoiceBox<School>(FXCollections.observableArrayList());
+			schoolSelect.getItems().add(null);
+			schoolSelect.getItems().addAll(Spell.School.values());
+			schoolSelect.setValue(null);
+	      	curSpell.add(schoolSelect, 1, 2);
+
+	      	label = new Label(" Classes");
+	      	curSpell.add(label, 0, 3);
+	      	GridPane classPanel = new GridPane();
+	      	classPanel.setHgap(10);
+	      		c1 = new RadioButton("Bard");
+	      		classPanel.add(c1, 0, 0);
+	      		c2 = new RadioButton("Cleric");
+	      		classPanel.add(c2, 1, 0);
+	      		c3 = new RadioButton("Druid");
+	      		classPanel.add(c3, 2, 0);
+	      		c4 = new RadioButton("Paladin");
+	      		classPanel.add(c4, 3, 0);
+	      		c5 = new RadioButton("Ranger");
+	      		classPanel.add(c5, 4, 0);
+	      		c6 = new RadioButton("Sorcerer");
+	      		classPanel.add(c6, 5, 0);
+	      		c7 = new RadioButton("Warlock");
+	      		classPanel.add(c7, 6, 0);
+	      		c8 = new RadioButton("Wizard");
+	      		classPanel.add(c8, 7, 0);
+	      	curSpell.add(classPanel, 1, 3);
+
+	      	label = new Label(" Source");
+	      	curSpell.add(label, 0, 4);
+			sourceSelect = new ChoiceBox<Source>(FXCollections.observableArrayList());
+			sourceSelect.getItems().add(null);
+			sourceSelect.getItems().addAll(Source.values());
+			sourceSelect.setValue(null);
+	      	curSpell.add(sourceSelect, 1, 4);
+	      	
+//	      	Apply button
+			Button add = new Button("Add spell");
+			add.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					Spell newSpell = new Spell() {
+						String name="";
+						int level=0;
+						School school = null;
+						List<Classes> classes = null;
+						List<Source> sources = null;
+						
+						public void constructor(String name, int level, School school, List<Classes> classes, List<Source> source){
+							this.name = name;
+							this.level = level;
+							this.school = school;
+							this.classes = classes;
+							this.sources = sources;
+						}
+
+						@Override public String getName() {return name;}
+						@Override public int getLevel() {return level;}
+						@Override public School getSchool() {return school;}
+						@Override public List<Classes> getClasses() {return classes;}
+						@Override public boolean fromClass(Classes curClass) {return classes.contains(curClass);}
+						@Override public boolean fromSource(Source source) {return sources.contains(source);}
+						
+						@Override public String toString() {return name;}
+					};
+					
+					List<Classes> classList = new ArrayList<Classes>();
+						if(c1.isSelected()){classList.add(Classes.BARD);}
+						if(c2.isSelected()){classList.add(Classes.CLERIC);}
+						if(c3.isSelected()){classList.add(Classes.DRUID);}
+						if(c4.isSelected()){classList.add(Classes.PALADIN);}
+						if(c5.isSelected()){classList.add(Classes.RANGER);}
+						if(c6.isSelected()){classList.add(Classes.SORCERER);}
+						if(c7.isSelected()){classList.add(Classes.WARLOCK);}
+						if(c8.isSelected()){classList.add(Classes.WIZARD);}
+					List<Source> sourcesList = new ArrayList<Source>();
+						sourcesList.add(sourceSelect.getValue());
+					newSpell.constructor(nameField.getText(), levelPicker.getValue(), schoolSelect.getValue(),
+							classList, sourcesList);
+					
+					System.out.println("Adding spell "+newSpell.getName());
+		      			Label label = new Label(" "+newSpell.getName());
+		      			Tooltip toolTip = new Tooltip(newSpell.toString());
+		      			label.setTooltip(toolTip);
+		      			names.add(label);
+		      			spellList.add(label, 0, spells.size()+1);
+		      			label = new Label(" "+newSpell.getLevel());
+		      			levels.add(label);
+		      			spellList.add(label, 1, spells.size()+1);
+					spells.add(newSpell);
+					updateSpellList();
+				}
+			});
+			curSpell.add(add, 0, 5);
+	      	
+	      	grid.add(curSpell, 1, 1);
+
+	      	
+	      	
 	    //make the window
-	    secondaryStage.setScene(new Scene(grid, 1200, 600));
+	    secondaryStage.setScene(new Scene(grid, 800, 600));
 		secondaryStage.show();
 		return secondaryStage;
 	}
 
 	private void updateSpellList() {
-		//TODO - update list for queries
-//		for(int i=0; i<spells.size(); i++) {
-//			boolean visible = query.contains(spells.get(i));
-//			TextField curField = fields.get(i);
-//			curField.setVisible(visible);//hide the text field for excluded creatures
-//			Label curLabel = fieldTags.get(i);
-//			curLabel.setVisible(visible);//hide its label
-//			if(!visible) {
-//				creatureList.getChildren().remove(curField);
-//				creatureList.getChildren().remove(curLabel);
-//			}else if(!(creatureList.getChildren().contains(curField)||creatureList.getChildren().contains(curLabel))){
-//				creatureList.getChildren().add(curField);
-//				creatureList.getChildren().add(curLabel);
-//			}
-//			
-//		}
+		for(int i=0; i<spells.size(); i++) {
+			boolean visible = query.contains(spells.get(i));
+			Label curName = names.get(i);
+			Label curLevel = levels.get(i);
+			curName.setVisible(visible);//hide the spell in the list for excluded creatures
+			curLevel.setVisible(visible);
+			if(!visible) {
+				spellList.getChildren().remove(curName);
+				spellList.getChildren().remove(curLevel);
+			}else if(!(spellList.getChildren().contains(curName)||spellList.getChildren().contains(curLevel))){
+				spellList.getChildren().add(curName);
+				spellList.getChildren().add(curLevel);
+			}			
+		}
 	}
 }
