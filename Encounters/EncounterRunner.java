e9079af5722da44cac1f4d2f699245ff53200d6b
russelbenj2 <ben+github@hardknocks.school.nz>
2019-03-16 23:19:15 +1300

    Set up health tracking and stat block display.
    Allocated space for health tracking.
    	Filled the area with sliders showing the hp range.
    	Set up buttons for removing rows.
    		If all of a init value are removed the init is removed.
    	Set up a way of applying damage (including type multipliers).
    Added dropdowns so stat blocks can be directly read.

--- Encounters/EncounterRunner.java
+++ Encounters/EncounterRunner.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 import Creatures.Creature;
+import Creatures.Creature.DamageType;
 import Creatures.Creature.Stats;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
@@ -19,9 +20,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ScrollPane;
 
 @SuppressWarnings("restriction")
@@ -29,11 +33,18 @@ public class EncounterRunner {
 	
 	private final List<Creature> creatures;
 	private Map<Creature,Integer> encounterCounts;
+	
 	private GridPane initiativeBar;
 		private List<GridPane> initiativePanes;
 		private Map<GridPane,Integer> initVals;
 		private Map<GridPane,Creature> initCreatures;
 		private GridPane lair=null;
+	
+	private GridPane healthBar;
+		private List<GridPane> healthPanes;
+		private Map<GridPane,GridPane> healthtoinit;
+	
+	private GridPane statsBar;
 
 	private RadioButton groupingToggle;
 	private int curTurn = -1;
@@ -91,7 +102,7 @@ public class EncounterRunner {
 			});
 			initiativeBar.add(newInit, 0, 0);
 			
-			groupingToggle = new RadioButton("Group");
+			groupingToggle = new RadioButton("Group\t\t");
 			groupingToggle.setSelected(true);
 			groupingToggle.setOnAction(new EventHandler<ActionEvent>() {
 				@Override public void handle(ActionEvent event) {updateCreatures();}});
@@ -112,12 +123,24 @@ public class EncounterRunner {
 		ScrollPane sp = new ScrollPane();
 		sp.setContent(initiativeBar);
 		grid.add(sp, 0, 1);
-				
+
+		healthBar = new GridPane();				//TODO - Label for the initiative bar.
+		sp = new ScrollPane();
+		sp.setContent(healthBar);
+		grid.add(sp, 0, 2);
+		
+			//Reactions spent, Recharge status, Legendaries (res and act) remaining
+			//Spell slots.
+		
+		statsBar = new GridPane();							//TODO - Label for the stat blocks bar.
+		sp = new ScrollPane();
+		sp.setContent(statsBar);
+		grid.add(sp, 0, 3);
 				
 				
 				
 
-	    secondaryStage.setScene(new Scene(grid, 800, 300));
+	    secondaryStage.setScene(new Scene(grid, 1200, 800));
 		secondaryStage.show();
 		return secondaryStage;
 	}
@@ -206,7 +229,8 @@ public class EncounterRunner {
 				}else{//Individual initiatives
 					for(int i=0; i<encounterCounts.get(c); i++){
 						GridPane newCreature = new GridPane();
-							Label name = new Label(c.getName()+" "+(i+1));
+							Label name = new Label(c.getName()+" "+(encounterCounts.get(c)-i));
+							if(encounterCounts.get(c)==1){name.setText(c.getName());}//Don't need suffix if alone.
 							newCreature.add(name, 0, 0);
 							TextField init = new TextField("");
 							newCreature.add(init, 0, 1);
@@ -228,12 +252,153 @@ public class EncounterRunner {
 				Label name = new Label(" Lair");
 				name.setMinWidth(70);
 				lair.add(name, 0, 0);
-				Label init = new Label(" 20 ");
+				Label init = new Label(" 20");
 				lair.add(init, 0, 1);
 			initVals.put(lair, 20);
 			initiativePanes.add(0, lair);
 		}
+		updateHealths();
 		updateInitBar();
+	}
+	
+	private void updateHealths(){							//TODO - Label for health bars when an encounter is loaded or grouping changes.
+		//Clear the old list
+		healthBar.getChildren().clear();
+		healthPanes = new ArrayList<GridPane>();
+		healthtoinit = new HashMap<GridPane,GridPane>();
+		//Add each bar
+		int i=0;
+		for(GridPane gp:initiativePanes){
+			if(initCreatures.containsKey(gp)){
+				if(groupingToggle.isSelected()){
+					for(int j=0; j<encounterCounts.get(initCreatures.get(gp)); j++){
+						addHealthBar(gp,(" "+initCreatures.get(gp).getName()+" "+(j+1)),i);
+						i++;
+					}
+				}else{
+					Label curLabel = null;
+					for(int j=0; j<gp.getChildren().size(); j++){
+						if(gp.getChildren().get(j) instanceof Label){
+							curLabel = (Label)gp.getChildren().get(j);
+						}
+					}//Use the label of the init being paired with.
+					addHealthBar(gp,(" "+curLabel.getText()),i);
+					i++;
+				}
+			}
+		}
+	}
+	private void addHealthBar(GridPane initPane, String label, int row){
+		GridPane healthRow = new GridPane();
+		healthBar.add(healthRow, 0, row);
+		healthPanes.add(healthRow);
+		healthtoinit.put(healthRow, initPane);
+		
+		Creature c = initCreatures.get(initPane);
+		Label nameLabel = new Label(label);
+		if(encounterCounts.get(c)==1){nameLabel.setText(" "+c.getName());}//Don't need suffix if alone.
+		nameLabel.setPrefWidth(200);
+		healthRow.add(nameLabel, 0, 0);
+		
+		Slider slider = new Slider();
+			slider.setMin(0);
+			slider.setMax(c.getHP());
+			slider.setValue(c.getHP());
+			slider.setShowTickLabels(true);
+			slider.setShowTickMarks(true);
+			slider.setMajorTickUnit(25);
+			slider.setMinorTickCount(5);
+			slider.setBlockIncrement(1);
+			slider.setSnapToTicks(true);
+		healthRow.add(slider, 1, 0);
+		
+		Label healthLabel = new Label("\t"+((int) slider.getValue())+" / "+c.getHP()+"\t");
+		healthRow.add(healthLabel, 2, 0);
+		slider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+				healthLabel.setText("\t"+((int) slider.getValue())+" / "+c.getHP()+"\t");
+            }
+        });
+		
+		TextField change = new TextField("0");
+		change.textProperty().addListener(makeIntFilter(change));
+		healthRow.add(change, 3, 0);
+		ChoiceBox<String> type = new ChoiceBox<String>();
+		type.getItems().addAll(null,"Healing");
+		for(DamageType d:DamageType.values()){type.getItems().add(d.toNiceString());}
+		healthRow.add(type, 4, 0);
+		change.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent event) {
+				//Determine health change
+				int damage = Integer.parseInt(change.getText());
+				if (type.getValue()!=null){
+					if(type.getValue().equals("Healing")){damage = -damage;}
+					else{
+						for(DamageType d:DamageType.values()){
+							if(d.toNiceString().equals(type.getValue())){
+								damage = (int) c.getMultiplier(d)*damage;
+							}
+						}
+					}
+				}
+				//Apply health change
+				//TODO tempHP
+				int postChange = (int) slider.getValue()-damage;
+				slider.setValueChanging(true);
+				if(postChange<0){slider.setValue(0);}
+				else if(postChange>c.getHP()){slider.setValue(c.getHP());}
+				else{slider.setValue(postChange);}
+				healthLabel.setText("\t"+((int) slider.getValue())+" / "+c.getHP()+"\t");
+			}
+		});
+
+		healthRow.add(new Label("\t\t"), 5, 0);
+		Button remove = new Button("Remove");
+		remove.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent arg0) {
+				//Remove from init bar if appropriate
+				boolean isLast = true;
+				for(GridPane other:healthtoinit.keySet()){
+					if(healthtoinit.get(other)==initPane && other!=healthRow){
+						isLast=false;
+					}
+				}
+				if(isLast){
+					initiativeBar.getChildren().remove(healthtoinit.get(healthRow));
+					initiativePanes.remove(healthtoinit.get(healthRow));
+					initVals.remove(healthtoinit.get(healthRow));
+					initCreatures.remove(healthtoinit.get(healthRow));
+				}
+				//Remove from health bar
+				healthBar.getChildren().remove(healthRow);
+				healthPanes.remove(healthRow);
+				healthtoinit.remove(healthRow);
+				//Remove from stat display if appropriate
+				encounterCounts.put(c, encounterCounts.get(c)-1);
+				updateStatDisplay();
+			}});
+		healthRow.add(remove, 6, 0);
+		
+		//TODO track stuff here.
+			//Track conditions
+			//Temp hp
+			//Bloodied colours?
+	}
+	
+	private void updateStatDisplay(){						//TODO - Label for printing statblocks.
+		statsBar.getChildren().clear();
+		int i=0;
+		for(Creature c:creatures){
+			if(encounterCounts.get(c)>0){
+				Label stat = new Label(c.toString());
+				stat.setWrapText(true);
+				TitledPane dd = new TitledPane(c.getName(),stat);
+				dd.setExpanded(false);
+				dd.setMaxWidth(600);
+				statsBar.add(dd, 0, i);
+				i++;
+			}
+		}
 	}
 	
 	private ChangeListener<String> makeIntFilter(TextField tf){
@@ -289,6 +454,7 @@ public class EncounterRunner {
 				scan.close();
 				System.out.println("Encounter loading complete");
 				updateCreatures();
+				updateStatDisplay();
 			} catch (FileNotFoundException e) {
 				System.out.println("Encounter loading failed");
 				e.printStackTrace();
